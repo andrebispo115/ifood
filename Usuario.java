2dd9e99f5de820f2de3a18c53d5d131030f2fa49
AndreBispo <andresvale2006@gmail.com>
2025-12-06 18:06:33 -0300

    Create abstract Usuario class with properties

---
+++ Usuario.java
@@ -0,0 +1,11 @@
+public abstract class Usuario {
+    protected String nome;
+    protected String email;
+
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+    
+    public String getNome() { return nome; }
+}
