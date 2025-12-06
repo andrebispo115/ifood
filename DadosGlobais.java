8dfa832f69a3ba7005c99508b75300f262aaeeab
AndreBispo <andresvale2006@gmail.com>
2025-12-06 18:01:31 -0300

    Update and rename Tela_cadastro.java to DadosGlobais.java

---
+++ DadosGlobais.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DadosGlobais {
+    public static List<Restaurante> restaurantes = new ArrayList<>();
+    public static Usuario usuarioLogado;
+
+    // O bloco "static" foi removido. 
+    // O sistema começa vazio e o usuário deve cadastrar o primeiro restaurante.
+}
