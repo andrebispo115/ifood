df4f75ea253a9e10165a44ac69d3af7bc795ba23
edgarvncs <edgar.vinicius.13@gmail.com>
2025-12-08 17:23:28 -0300

    Update Restaurante.java

--- Restaurante.java
+++ Restaurante.java
@@ -4,44 +4,52 @@ import java.util.List;
 public class Restaurante implements Gerenciavel {
     private String nome;
     private String endereco;
+    // Geolocalização (Ponto Extra)
+    private int posX; 
+    private int posY;
+    
     private List<Produto> cardapio;
 
-    public Restaurante(String nome, String endereco) {
+    public Restaurante(String nome, String endereco, int posX, int posY) {
         this.nome = nome;
         this.endereco = endereco;
+        this.posX = posX;
+        this.posY = posY;
         this.cardapio = new ArrayList<>();
     }
 
-    // Implementação da Interface
+    // Método Matemático para Geolocalização (Distância Euclidiana)
+    public double calcularDistancia(int clienteX, int clienteY) {
+        // Fórmula: RaizQuadrada((x2-x1)² + (y2-y1)²)
+        return Math.sqrt(Math.pow(this.posX - clienteX, 2) + Math.pow(this.posY - clienteY, 2));
+    }
+
+    public double calcularFrete(int clienteX, int clienteY) {
+        double distancia = calcularDistancia(clienteX, clienteY);
+        return distancia * 1.50; // Cobra R$ 1,50 por Km de distância
+    }
+
     @Override
     public void adicionarProduto(Produto p) {
         cardapio.add(p);
-        System.out.println("Produto adicionado: " + p.getNome());
     }
 
     @Override
     public void removerProduto(Produto p) {
         cardapio.remove(p);
-        System.out.println("Produto removido: " + p.getNome());
     }
 
     @Override
     public void listarCardapio() {
-        System.out.println("--- Cardápio do " + nome + " ---");
-        for (Produto p : cardapio) {
-            System.out.println(p); // Chama o toString polimórfico
-        }
+        for (Produto p : cardapio) System.out.println(p);
     }
     
-    // Getters
     public List<Produto> getCardapio() { return cardapio; }
     public String getNome() { return nome; }
-    public String getEndereco() {
-    return endereco;
-}
-@Override
-public String toString() {
-    // Usando o 'endereco' para o aviso sumir
-    return nome + " (" + endereco + ")";
-}
+    public String getEndereco() { return endereco; }
+    
+    @Override
+    public String toString() {
+        return nome + " (Loc: " + posX + ", " + posY + ")";
+    }
 }
